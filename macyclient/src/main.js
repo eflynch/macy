82b885b56046654c889a00984bef7e5f4d2d46b6
Evan Lynch <elynch@izotope.com>
2020-03-03 20:34:29 -0500

    Clean up main

--- macyclient/src/main.js
+++ macyclient/src/main.js
@@ -5,15 +5,13 @@ import App from './components/app';
 import Store from './store';
 import Actions from './actions';
 
-import {generate} from './board-specs/trad';
-window.generate = generate;
 
 const main = () => {
     Store.addOnChange(() => {
         const state = Store.getState();
         render(<App {...state} />, document.getElementById("content"));
     });
-    Actions.loadSession("testSession2.json");
+    Actions.loadSession("testSession.json");
 };
 
 
